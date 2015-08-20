b874f7ed69a9ae3a275f28b5d1baf1665da227c7
Ken Duck <ken@qracorp.com>
2015-08-20 19:04:44 -0400

    Initial Commit

---
+++ plugins/net.ossindex.common/src/net/ossindex/common/resource/AbstractRemoteResource.java
@@ -0,0 +1,97 @@
+/**
+ *	Copyright (c) 2014-2015 Vör Security Inc.
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	    * Redistributions of source code must retain the above copyright
+ *	      notice, this list of conditions and the following disclaimer.
+ *	    * Redistributions in binary form must reproduce the above copyright
+ *	      notice, this list of conditions and the following disclaimer in the
+ *	      documentation and/or other materials provided with the distribution.
+ *	    * Neither the name of the <organization> nor the
+ *	      names of its contributors may be used to endorse or promote products
+ *	      derived from this software without specific prior written permission.
+ *	
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ossindex.common.resource;
+
+
+/** Provides common code for OSS Index API access.
+ * 
+ * @author Ken Duck
+ *
+ */
+public abstract class AbstractRemoteResource
+{
+	private static final boolean DEBUG = true;
+	/**
+	 * Bad form! Hard-coded for prototype.
+	 */
+	private static String scheme = "https";
+	private static String host = "ossindex.net";
+	private static int port = -1; // Use default port
+	
+	/**
+	 * DEBUG coordinates
+	 */
+	static
+	{
+		if(DEBUG)
+		{
+			scheme = "http";
+			host = "localhost";
+			port = 8080;
+		}
+	}
+	
+	/**
+	 * OSS Index ID. Special values:
+	 * 
+	 *   >0 Valid OSS Index resource ID.
+	 *   -1 OSS Index not checked
+	 *   -2 Resource not in OSS Index
+	 */
+	private long id = -1;
+	
+	/** Get the OSS Index ID.
+	 * 
+	 * @return
+	 */
+	public long getId()
+	{
+		return id;
+	}
+	
+	/** Get the base URL for REST requests.
+	 * 
+	 * @return
+	 */
+	protected static String getBaseUrl()
+	{
+		if(port >= 0)
+		{
+			return scheme + "://" + host + ":" + port;
+		}
+		else
+		{
+			return scheme + "://" + host;
+		}
+	}
+	
+	/** Get the OSS Index resource type.
+	 * 
+	 * @return
+	 */
+	protected abstract String getResourceType();
+}
---
+++ plugins/net.ossindex.common/src/net/ossindex/common/resource/FileResource.java
@@ -0,0 +1,127 @@
+/**
+ *	Copyright (c) 2014-2015 Vör Security Inc.
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	    * Redistributions of source code must retain the above copyright
+ *	      notice, this list of conditions and the following disclaimer.
+ *	    * Redistributions in binary form must reproduce the above copyright
+ *	      notice, this list of conditions and the following disclaimer in the
+ *	      documentation and/or other materials provided with the distribution.
+ *	    * Neither the name of the <organization> nor the
+ *	      names of its contributors may be used to endorse or promote products
+ *	      derived from this software without specific prior written permission.
+ *	
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ossindex.common.resource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+/** Representation of the FileResource, backed by the OSS Index REST API
+ * 
+ * @author Ken Duck
+ *
+ */
+@SuppressWarnings("restriction")
+public class FileResource extends AbstractRemoteResource
+{
+	/**
+	 * File name. May be populated by OSS Index, but can be
+	 * overridden by local file.
+	 */
+	private String name = null;
+	
+	/** The empty constructor should only be accessed internally.
+	 * 
+	 * @param file
+	 */
+	private FileResource()
+	{
+	}
+	
+	/** Find an applicable resource, otherwise return null. Use a combination
+	 * of HttpClient and GSON to handle the request and response.
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException 
+	 */
+	public static FileResource find(File file) throws IOException
+	{
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		String requestString = getBaseUrl() + "/rest/sha1/" + getSha1(file);
+		HttpGet request = new HttpGet(requestString);
+		CloseableHttpResponse response = httpClient.execute(request);
+		String json = EntityUtils.toString(response.getEntity(), "UTF-8");
+		System.err.println(getSha1(file) + " " + json);
+		Gson gson = new Gson();
+		try
+		{
+		FileResource result = gson.fromJson(json, FileResource.class);
+		if(result.getId() > 0) return result;
+		}
+		catch(JsonSyntaxException e)
+		{
+			System.err.println("Exception parsing response from request '" + requestString + "'");
+		}
+        return null;
+	}
+
+	/** Get the SHA1 checksum for the file.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private static String getSha1(File file) throws IOException
+	{
+		// Get the SHA1 sum for a file, then check if the MD5 is listed in the
+		// OSS Index (indicating it is third party code).
+		FileInputStream is = null;
+		try
+		{
+			is = new FileInputStream(file);
+			return DigestUtils.shaHex(is);
+		}
+		finally
+		{
+			if(is != null)
+			{
+				is.close();
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see net.ossindex.common.resource.AbstractRemoteResource#getResourceType()
+	 */
+	@Override
+	protected String getResourceType()
+	{
+		return "file";
+	}
+	
+}
